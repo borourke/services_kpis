0d5301005bdcc8b3604bc888df2d11d9bc08575c
borourke <borourke@crowdflower.com>
2014-07-09 13:24:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ServicesKpis::Application.config.secret_key_base = 'be1ef53ef1f75c5af558edf0115c2c2edb185b26d8a3433502dc7f237a8628d5078f534360f717503622de7416fd5f18cc0297ce1744c11d3a08c28ded9090a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ServicesKpis::Application.config.session_store :cookie_store, key: '_services_kpis_session'
