5e427aad2b994abe2b6a77e2ae202dc2cdec0702
borourke <borourke@crowdflower.com>
2014-09-25 15:50:54 -0700

    fixing happiness guage

--- app/models/happiness_survey.rb
+++ app/models/happiness_survey.rb
@@ -124,8 +124,6 @@ class HappinessSurvey < ActiveRecord::Base
                 :stamina, :growth, :development)
     surveys.flatten!
     avg = surveys.inject{ |sum, el| sum + el }.to_f / surveys.size
-    avg = 1
-    percent = 0
     percent = (avg / 5.0 * 100).round
   end
 
